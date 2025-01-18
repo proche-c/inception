1753b42968866f8c401c37faa8688b3fa61e7ce2
Paula Roche <paularoche@Air-de-Paula.home>
2025-01-18 13:37:45 +0100

    1

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// Código PHP para el archivo index.php
+
+// Aquí se pueden definir variables, funciones y realizar otros preparativos
+
+// Ejemplo de generación de contenido dinámico
+$nombre = "Paula";
+
+$mensaje = "¡Hola, $nombre!";
+
+// La salida generada se enviará al navegador del usuario
+echo $mensaje;
+?>
